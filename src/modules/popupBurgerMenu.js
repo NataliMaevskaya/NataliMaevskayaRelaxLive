930edb40e10de5858a757bff48ac82a9580aa955
Natali.Maevskaya <natali.maevskaya@gmail.com>
2019-10-05 17:09:35 +0300

    Бургер меню и адаптив(моб. меню сверху)

--- src/modules/popupBurgerMenu.js
+++ src/modules/popupBurgerMenu.js
@@ -1,17 +1,22 @@
 const popupBurgerMenu = () => {
-    const menuIcon = document.querySelector('.menu__icon'),
-        popupDialogMenu = document.querySelector('.popup-dialog-menu'),
-        closeMenu = document.querySelector('.close-menu');
-    
-        menuIcon.addEventListener('click', () => {            
-            popupDialogMenu.style.cssText = `transform: translate3d(0,0,0) !important;
-                                            -webkit-transform: translate3d(0,0,0) !important;`;
-            if (document.documentElement.clientWidth < 576) {
-                popupDialogMenu.style.cssText += `width: 105%;`;
-            } 
-        });
-        closeMenu.addEventListener('click', () => {
-            popupDialogMenu.removeAttribute('style');
+    const body = document.querySelector('body'),
+        popupDialogMenu = document.querySelector('.popup-dialog-menu');
+
+        body.addEventListener('click', (event) => {
+            let target = event.target;
+            if (target.closest('.menu__icon')) {
+                popupDialogMenu.style.cssText = `transform: translate3d(0,0,0) !important;
+                                                -webkit-transform: translate3d(0,0,0) !important;`;
+                if (document.documentElement.clientWidth < 576) {
+                    popupDialogMenu.style.cssText += `width: 105%;`;
+                } 
+            } else if (popupDialogMenu.getAttribute('style')) {
+                if (target.closest('.close-menu') || 
+                   (target.closest('body') && !target.closest('.popup-dialog-menu')) ) {  
+                       popupDialogMenu.removeAttribute('style');
+                }
+            }
+            
         });
 };  
 export default popupBurgerMenu;
